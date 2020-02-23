9facc560c929a006dfa1f2926df5d36d370a5f56
rainer.telesko <rainer.telesko@fhnw.ch>
2020-02-23 10:29:47 +0100

    Fourth commit

--- app/src/main/java/com/example/lotterycamera/MainActivity.java
+++ app/src/main/java/com/example/lotterycamera/MainActivity.java
@@ -26,15 +26,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btCamera = findViewById(R.id.btCamera);
         ivPhoto = findViewById(R.id.ivPhoto);
-
-        // Camera_open button is for open the camera
-        // and add the setOnClickListener in this button
+        /*
+        Camera_open button is for open the camera
+        and add the setOnClickListener in this button
+        */
         btCamera.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // Create the camera_intent ACTION_IMAGE_CAPTURE
-                // it will open the built-in camera for capturing the image
+                /*
+                Create the camera_intent ACTION_IMAGE_CAPTURE
+                it will open the built-in camera for capturing the image
+                */
                 Intent camera_intent
                         = new Intent(MediaStore
                         .ACTION_IMAGE_CAPTURE);
@@ -54,9 +57,11 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         // Match the request 'pic id' with requestCode and resultCode
         if (requestCode == PIC_ID && resultCode == RESULT_OK) {
-            // BitMap is a data structure of image file
-            // which stores the image in memory
-            // Disadvantage: low resolution quality (only Thumbnail)
+            /*
+            BitMap is a data structure of image file
+            which stores the image in memory
+            Disadvantage: low resolution quality (only Thumbnail)
+            */
             if (data.getExtras() != null) {     // Check if Intent is empty
                 Bitmap photo = (Bitmap) data.getExtras().get("data");
                 // Set the image in ImageView for display
